09f6751fbb8a0ea49032c25a9ad06a8f71532e93
Ximo Llacer <xllaber7@gmail.com>
2023-10-26 12:40:04 +0200

    Dont know what this is

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,7 +12,7 @@ export class SpotifyService {
 
   getQuery(query: string) {
     const headers = new HttpHeaders({
-      "Authorization": "Bearer BQBr4xgUb4hFupFmk79eKrnF2fzxc23N5Cil6X31026LTYfG48rzWsRclNagHn5bTkY8aRo19U5bqWgR0tMsG33_TjzX8PIqQbjFsWMxN_kn7Pf2EwU"
+      "Authorization": "Bearer BQAeEgt_bTT2abEnkVzyvwksSIx5IF5hcumSu6ADB34uUA950jaWxeSV4ltmL0wg7dvu_bku1uHZhNeNuPeKEsVu3HI-PU62QegegI6EZtp9erzdU-s"
     })
     const url = `https://api.spotify.com/v1/${query}`;
     return this.http.get(url, {headers});
